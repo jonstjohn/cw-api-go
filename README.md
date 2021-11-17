ed90c9981d535bfee6e1775f7da0a88e23a4abff
Jon St John <jon@cockroachlabs.com>
2021-11-16 17:36:52 -0700

    Fixed the endpoints

--- README.md
+++ README.md
@@ -6,3 +6,12 @@ ClimbingWeather.com API service written in Go
 ```
 gcloud run deploy cw-api-go --region us-central1 --allow-unauthenticated --source=.
 ```
+
+## Database URLs
+
+gcloud sql instances describe cw-pg-dev | grep connectionName
+Pg: user=cwapp password=XXXXXXX database=cw host=/cloudsql/api-project-736062072361:us-central1:cw-pg-dev
+CRDB: postgresql://cwapp:XXXXX@free-tier.gcp-us-central1.cockroachlabs.cloud:26257/cw?sslmode=verify-full&sslrootcert=/certs/CW_CRDB_CRT&options=--cluster%3Dcw-test-4902
+
+Squamish
+INSERT INTO area (name, latitude, longitude) values ('Squamish', 49.7016, -123.1558);
--- main.go
+++ main.go
@@ -33,15 +33,80 @@ func main() {
 		defer conn.Close(context.Background())
 
 		var name string
-		err = conn.QueryRow(context.Background(), "select name from area where area_id=$1", areaId).Scan(&name)
+		var latitude float32
+		var longitude float32
+		err = conn.QueryRow(context.Background(), "select name, latitude, longitude from area where area_id=$1", areaId).Scan(&name, &latitude, &longitude)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
 
+		/*
+			rows, err := conn.Query(context.Background(),
+				"SELECT date, high, low, precip_day/100, precip_night/100, sky/100, relative_humidity/100,"+
+					" wsym, rain_amount, snow_amount, wind_sustained, wind_gust, weather"+
+					" FROM daily WHERE area_id = $1 ORDER BY date asc", areaId)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+				return
+			}
+			defer rows.Close()
+
+			daily := make(map[string]interface{})
+			daily["summary"] = ""
+			data := make([]map[string]interface{}, 0)
+			for rows.Next() {
+				var date time.Time
+				var high int
+				var low int
+				var precipDay float32
+				var precipNight float32
+				var sky float32
+				var humidity float32
+				var wsym string
+				var rainAmount float32
+				var snowAmount float32
+				var windSustained int
+				var windGust int
+				var weather string
+				//values, err := rows.Values()
+				rows.Scan(&date, &high, &low, &precipDay, &precipNight, &sky, &humidity, &wsym, &rainAmount, &snowAmount, &windSustained, &windGust, &weather)
+				day := make(map[string]interface{})
+				day["time"] = date.Unix()
+				day["temperatureHigh"] = high
+				day["temperatureLow"] = low
+				day["precipProbability"] = precipDay
+				day["precipProbabilityNight"] = precipNight
+				day["humidity"] = humidity
+				day["windSpeed"] = windSustained
+				day["windGust"] = windGust
+				data = append(data, day)
+			}
+		*/
+
+		/*
+			daily["summary"] = ""
+			data := make([]map[string]interface{}, 0)
+			day0 := make(map[string]interface{})
+			day0["time"] = 1636970400
+			day0["temperatureHigh"] = 72
+			day0["temperatureLow"] = 51
+			day0["precipProbability"] = 0.18
+			day0["precipProbabilityNight"] = 0.20
+			day0["humidity"] = 0.72
+			day0["windSpeed"] = 2
+			day0["windGust"] = 9
+			data = append(data, day0)
+		*/
+		//daily["data"] = data
+
 		c.JSON(200, gin.H{
-			"message": name,
+			"name":      name,
+			"latitude":  latitude,
+			"longitude": longitude,
+			//"daily":     daily,
 		})
 	})
 	port := os.Getenv("PORT")
